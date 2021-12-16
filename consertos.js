63d7e513b1c4a73271e29865335c710cbddac0f8
Paulo Henrique Costa <paulohenriquefcosta@gmail.com>
2021-12-15 21:16:26 -0300

    terceiro desafio

--- consertos.js
+++ consertos.js
@@ -3,7 +3,7 @@ class Remanufaturados {
     listaObjetos = [];
     aparelho = "";
     itensConsertados = [];
-    valores = 0;
+    valores = [];
     pronto = true;
   
     // Criação do construtor para garantir que poderão ser utilizados em mais de um método (pensando em escalabilidade do código).
@@ -29,29 +29,25 @@ class Remanufaturados {
   const novoAparelho = new Remanufaturados();
   
   //   LISTA COM APARELHOS
-
-
-
-    novoAparelho.adicionarAparelho("Xen Money o.O", ["tela", "bateria"], (190,200), true);
-    novoAparelho.adicionarAparelho("Um rim -_-", ["tela"], (350), true);
-    novoAparelho.adicionarAparelho("+ouBenos 00a", ["placa mãe"], (800), false);
-    novoAparelho.adicionarAparelho("SóLiga a1212", ["placa de áudio"], (178), true);
-    novoAparelho.adicionarAparelho("Xen Money PRO <0-0>", ["carregador", "fone"], (75,97), true);
-    novoAparelho.adicionarAparelho("Tijolão", ["bateria"], (500), false); //esperando bateria chegar (em falta com fornecedor)
-    novoAparelho.adicionarAparelho("Celulator", ["tela", "bateria"], (300,97), false);
-    novoAparelho.adicionarAparelho("AloU", ["tela"], (173), false);
-    novoAparelho.adicionarAparelho("Xen Money -.O", ["bateria"], (200), true);
-    novoAparelho.adicionarAparelho("Parece bom ■■", ["tela"], (250),true);
-    novoAparelho.adicionarAparelho("+ouBenos 0B3", ["placa mãe"], (900), true);
-    novoAparelho.adicionarAparelho("NemLiga", ["tela"], (230), true);
-    novoAparelho.adicionarAparelho("Xen Money PRO+ 0-0", ["carregador", "fone"], (130,50), false);
-    novoAparelho.adicionarAparelho("Tijolinho", ["bateria"], (320), true); 
-    novoAparelho.adicionarAparelho("NumDuraUmAno", ["placa mãe"], (400), true);
-    novoAparelho.adicionarAparelho("PareceQueVai", ["tela"], (190), true);
+    novoAparelho.adicionarAparelho("Xen Money o.O", ["tela", "bateria"], [190,200], true);
+    novoAparelho.adicionarAparelho("Um rim -_-", ["tela"], [350], true);
+    novoAparelho.adicionarAparelho("+ouBenos 00a", ["placa mãe"], [800], false);
+    novoAparelho.adicionarAparelho("SóLiga a1212", ["placa de áudio"], [178], true);
+    novoAparelho.adicionarAparelho("Xen Money PRO <0-0>", ["carregador", "fone"], [75,97], true);
+    novoAparelho.adicionarAparelho("Tijolão", ["bateria"], [500], false); //esperando bateria chegar (em falta com fornecedor)
+    novoAparelho.adicionarAparelho("Celulator", ["tela", "bateria"], [300,97], false);
+    novoAparelho.adicionarAparelho("AloU", ["tela"], [173], false);
+    novoAparelho.adicionarAparelho("Xen Money -.O", ["bateria"], [200], true);
+    novoAparelho.adicionarAparelho("Parece bom ■■", ["tela"], [250],true);
+    novoAparelho.adicionarAparelho("+ouBenos 0B3", ["placa mãe"], [900], true);
+    novoAparelho.adicionarAparelho("NemLiga", ["tela"], [230], true);
+    novoAparelho.adicionarAparelho("Xen Money PRO+ 0-0", ["carregador", "fone"], [130,50], false);
+    novoAparelho.adicionarAparelho("Tijolinho", ["bateria"], [320], true); 
+    novoAparelho.adicionarAparelho("NumDuraUmAno", ["placa mãe"], [400], true);
+    novoAparelho.adicionarAparelho("PareceQueVai", ["tela"], [190], true);
 
     
   
-
     // comando para testar a visualização de um aparelho da lista
     // console.log(novoAparelho.listaObjetos[1]);
 
--- manipulacoes.js
+++ manipulacoes.js
@@ -6,9 +6,39 @@ const novoAparelho = require('./consertos');
 const celularesProntosProAbraco = novoAparelho.listaObjetos.filter (item => item.pronto == true)
 const celularesEmBanhoMaria = novoAparelho.listaObjetos.filter (item => item.pronto == false)
 
+// // para ver a lista de celulares prontos
+// console.log('\n Abaixo a lista de todos os aparelhos prontos para retirada \n')
 
-console.log(celularesProntosProAbraco);
-console.log(celularesEmBanhoMaria);
+// console.log(celularesProntosProAbraco);
+
+// para ver a lista de celulares ainda em conserto
+// console.log(celularesEmBanhoMaria);
 
 
-// reduce
+
+function somaProdutos(valores)
+{
+    const total = valores.reduce((total, valor) => total + valor);
+    return console.log(`Valor total do conserto ${total}`)
+};
+
+// console.log(somaProdutos()); acho que não vai servir
+
+celularesProntosProAbraco.forEach(item => {
+    console.log(`Aparelho: ${item.aparelho}`);
+
+    console.log(`Itens consertados: `);
+    console.log(`* ${item.itensConsertados}`);
+
+    somaProdutos(item.valores);
+
+    console.log('\n');
+});
+
+
+
+
+
+
+
+
